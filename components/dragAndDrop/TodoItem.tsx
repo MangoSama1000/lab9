808ae768e35fbabc3ca4a839e59e39e5795835fb
MangoSama1000 <tilija_nitej@hotmail.com>
2024-10-14 23:25:06 +0100

    final

--- components/dragAndDrop/TodoItem.tsx
+++ components/dragAndDrop/TodoItem.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-nested-ternary */
 import React, { useState } from 'react';
 import { Card, CardContent, Typography, Chip, Divider, IconButton, Box } from '@mui/material';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
